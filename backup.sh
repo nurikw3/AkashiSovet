670182a4d9727d90770889fa7b8f739cab13676c
nurikw3 <nurasyl1308ok@gmail.com>
2026-05-06 11:32:16 +0500

    backup init

--- backup.sh
+++ backup.sh
@@ -1,33 +1,10 @@
 #!/bin/bash
+DATE=$(date +%Y%m%d_%H%M%S)
+BACKUP_DIR="./backups/postgres"
+mkdir -p $BACKUP_DIR
 
-# Настройки
-CONTAINER_NAME="akashi_db"
-DB_USER="akashi"
-DB_NAME="akashi"
-BACKUP_DIR="./backups"
-DAYS_TO_KEEP=7
+docker exec akashi_db pg_dump -U akashi akashi | gzip > $BACKUP_DIR/akashi_$DATE.sql.gz
 
-# Создаем папку для бекапов, если её нет
-mkdir -p "$BACKUP_DIR"
+find $BACKUP_DIR -name "*.sql.gz" -mtime +30 -delete
 
-# Генерируем имя файла с текущей датой и временем
-DATE=$(date +%Y-%m-%d_%H-%M-%S)
-FILE_NAME="$BACKUP_DIR/${DB_NAME}_backup_${DATE}.sql.gz"
-
-echo "Начинаем создание бекапа БД..."
-
-# Выполняем дамп внутри контейнера и сразу сжимаем его
-docker exec -t $CONTAINER_NAME pg_dump -U $DB_USER -d $DB_NAME | gzip > "$FILE_NAME"
-
-if [ $? -eq 0 ]; then
-  echo "✅ Бекап успешно создан: $FILE_NAME"
-else
-  echo "❌ Ошибка при создании бекапа!"
-  exit 1
-fi
-
-# Удаляем старые бекапы (старше $DAYS_TO_KEEP дней)
-echo "Очистка старых бекапов (оставляем за последние $DAYS_TO_KEEP дней)..."
-find "$BACKUP_DIR" -type f -name "*.sql.gz" -mtime +$DAYS_TO_KEEP -exec rm {} \;
-
-echo "Готово."
+echo "Backup done: akashi_$DATE.sql.gz"
